837967813fee77515ee910aac650e0f9df521280
redfieldstefan <sredfield2@lululemon.com>
2023-03-23 10:13:53 -0700

    add 2 20 23, webpack logic, start dailys

---
+++ scripts/backgrounder.js
@@ -0,0 +1,30 @@
+import hexContrastColor from 'hex-contrast-color';
+import rgbHex from 'rgb-hex';
+
+const findBackgroundColor = (element) => {
+  const backgroundColor = window.getComputedStyle(element).getPropertyValue('background-color');
+  const hex = rgbHex(backgroundColor);
+  const isClear = hex === "00000000"
+
+  console.log({isClear, hex})
+  if (isClear && element.parentElement) {
+    return findBackgroundColor(element.parentElement)
+  }
+  
+  return `#${hex}`;
+};
+
+const textify = ((id) => {
+  const texts = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a'].map((tag) => document.getElementsByTagName(tag)).filter(arr => arr.length);
+  
+  texts.forEach(group => {
+    for (let element of group) {
+      const color = hexContrastColor(findBackgroundColor(element));
+      element.style.color = color;
+    }
+  
+    return
+  });
+})();
+
+export default textify;
