68d5dca68b817efda6de84d9ef870b32cdfd7e18
redfieldstefan <sredfield2@lululemon.com>
2023-03-28 15:21:24 -0700

    2-27-2023, some random element creation and placement

---
+++ scripts/blocky-griddy-patterny.js
@@ -0,0 +1,49 @@
+import hexContrastColor from 'hex-contrast-color';
+import rgbHex from 'rgb-hex';
+
+const randomWholeNum = (max = 0) => {
+  return Math.floor(Math.random() * max);
+};
+
+const blockyGriddyPatterny = (() => {
+  const min = 10;
+  const max = 20;
+  const maxY = window.innerHeight;
+  const maxX = window.innerWidth;
+
+  console.log({maxY, maxX})
+
+  const body = document.getElementsByTagName('body')[0];
+
+  const howManyBlockyOnes = Math.floor(Math.random() * (max - min) + min);
+
+  console.log({howManyBlockyOnes})
+
+  const newBlockyArray = new Array(howManyBlockyOnes).fill(null);
+
+  console.log({newBlockyArray})
+
+  const newBlockyOnes = newBlockyArray.map((index) => {
+    const newDiv = document.createElement("div");
+    const myShadow = `0px 0px ${randomWholeNum(50)}px ${randomWholeNum(100)}px rgb(0, 0, 0)`;
+    newDiv.className="blocky-one"
+    newDiv.style.position = 'absolute';
+    newDiv.style.top = randomWholeNum(maxY);
+    newDiv.style.right = randomWholeNum(maxX);
+    newDiv.style.height = randomWholeNum(1000);
+    newDiv.style.transform = `skew(${randomWholeNum(360)}deg)`;
+    newDiv.style["box-shadow"] = `${myShadow}`;
+    newDiv.style["-webkit-box-shadow"] = `${myShadow}`;
+    newDiv.style["-moz-box-shadow"] = `${myShadow}`;
+
+
+
+    return newDiv;
+  });
+
+  newBlockyOnes.forEach(one => body.appendChild(one));
+
+  return;
+})();
+
+export default blockyGriddyPatterny;
